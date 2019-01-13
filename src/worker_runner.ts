ad06f17122ba0bcd0b6c4d788ad13d2d671314cd
Divam <dfordivam@gmail.com>
2019-01-13 12:12:01 +0900

    Add JSaddleDevice

--- src/worker_runner.ts
+++ src/worker_runner.ts
@@ -1,7 +1,12 @@
 import { configureFileSystem, Process } from "./index";
 import { connectParent } from "./worker";
+import { JSaddleDevice } from "./JSaddleDevice";
 
 connectParent({ onMessage: async msg => {
-  const fs = await configureFileSystem({ devices: {} });
-  (await Process.instantiateProcess(fs, msg)).start();
+  const jsaddleDevice = new JSaddleDevice(
+    msg.data.jsaddleListener,
+    msg.data.jsaddleMsgBufArray,
+    msg.data.jsaddleMsgBufArray32);
+  const fs = await configureFileSystem({ devices: { ["jsaddle_inout"]: jsaddleDevice } });
+  (await Process.instantiateProcess(fs, msg.data.url)).start();
 }});
