3408d03b5eac246ecfaa7edcbd194efbc24c5eaf
Divam <dfordivam@gmail.com>
2020-01-15 19:06:15 +0900

    add jsaddle helper functions

--- jsaddleJS/jsaddle.js
+++ jsaddleJS/jsaddle.js
@@ -279,6 +279,130 @@ function jsaddleHandler(msg) {
   runBatch(batch);
 }
 
+// ghcjs helper functions
+function h$isNumber(o) {
+    return typeof(o) === 'number';
+}
+
+// returns true for null, but not for functions and host objects
+function h$isObject(o) {
+    return typeof(o) === 'object';
+}
+
+function h$isString(o) {
+    return typeof(o) === 'string';
+}
+
+function h$isSymbol(o) {
+    return typeof(o) === 'symbol';
+}
+
+function h$isBoolean(o) {
+    return typeof(o) === 'boolean';
+}
+
+function h$isFunction(o) {
+    return typeof(o) === 'function';
+}
+
+function h$jsTypeOf(o) {
+    var t = typeof(o);
+    if(t === 'undefined') return 0;
+    if(t === 'object')    return 1;
+    if(t === 'boolean')   return 2;
+    if(t === 'number')    return 3;
+    if(t === 'string')    return 4;
+    if(t === 'symbol')    return 5;
+    if(t === 'function')  return 6;
+    return 7; // other, host object etc
+}
+
+function h$jsonTypeOf(o) {
+    if (!(o instanceof Object)) {
+        if (o == null) {
+            return 0;
+        } else if (typeof o == 'number') {
+            if (h$isInteger(o)) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else if (typeof o == 'boolean') {
+            return 3;
+        } else {
+            return 4;
+        }
+    } else {
+        if (Object.prototype.toString.call(o) == '[object Array]') {
+            // it's an array
+            return 5;
+        } else if (!o) {
+            // null
+            return 0;
+        } else {
+            // it's an object
+            return 6;
+        }
+    }
+
+}
+function h$roundUpToMultipleOf(n,m) {
+  var rem = n % m;
+  return rem === 0 ? n : n - rem + m;
+}
+
+function h$newByteArray(len) {
+  var len0 = Math.max(h$roundUpToMultipleOf(len, 8), 8);
+  var buf = new ArrayBuffer(len0);
+  return { buf: buf
+         , len: len
+         , i3: new Int32Array(buf)
+         , u8: new Uint8Array(buf)
+         , u1: new Uint16Array(buf)
+         , f3: new Float32Array(buf)
+         , f6: new Float64Array(buf)
+         , dv: new DataView(buf)
+         }
+}
+function h$wrapBuffer(buf, unalignedOk, offset, length) {
+  if(!unalignedOk && offset && offset % 8 !== 0) {
+    throw ("h$wrapBuffer: offset not aligned:" + offset);
+  }
+  if(!buf || !(buf instanceof ArrayBuffer))
+    throw "h$wrapBuffer: not an ArrayBuffer"
+  if(!offset) { offset = 0; }
+  if(!length || length < 0) { length = buf.byteLength - offset; }
+  return { buf: buf
+         , len: length
+         , i3: (offset%4) ? null : new Int32Array(buf, offset, length >> 2)
+         , u8: new Uint8Array(buf, offset, length)
+         , u1: (offset%2) ? null : new Uint16Array(buf, offset, length >> 1)
+         , f3: (offset%4) ? null : new Float32Array(buf, offset, length >> 2)
+         , f6: (offset%8) ? null : new Float64Array(buf, offset, length >> 3)
+         , dv: new DataView(buf, offset, length)
+         };
+}
+function h$newByteArrayFromBase64String(base64) {
+  var bin = window.atob(base64);
+  var ba = h$newByteArray(bin.length);
+  var u8 = ba.u8;
+  for (var i = 0; i < bin.length; i++) {
+    u8[i] = bin.charCodeAt(i);
+  }
+  return ba;
+}
+function h$byteArrayToBase64String(off, len, ba) {
+  var bin = '';
+  var u8 = ba.u8;
+  var end = off + len;
+  for (var i = off; i < end; i++) {
+    bin += String.fromCharCode(u8[i]);
+  }
+  return window.btoa(bin);
+}
+
+
+//
 // Communication with JSaddleDevice running in the webabi webworker
 
 // Webabi Device -> JS
